ae1e8dd28e96ce8ccd40f1f44f2eaa17108341b3
Fergus Leahy <fergus.leahy@gmail.com>
2016-07-21 13:12:29 +0100

    Added URL GET parameter parsing  - parseRequest takes a socket and returns a request struct filled with params

--- core.c
+++ core.c
@@ -10,7 +10,23 @@
 #include <sys/types.h>
 #include <time.h>
 
-#define SERVER_NAME "CWeb"
+#define REQUEST_SIZE 1500
+
+typedef struct param {
+  char *key;
+  char *value;
+  struct param *next;
+} Param;
+
+typedef struct request {
+    char buf[REQUEST_SIZE];
+    char *type;
+    char *url;
+    char *paramStr;
+    Param *param;
+} Request;
+
+#define SERVER_NAME "ChibiCWeb"
 char const *msg = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: CWeb (MacOS)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 0\nContent-Type: text/html\nConnection: Closed";
 
 
@@ -37,58 +53,127 @@ int http_response(int fd, int status, char *text, int len) {
   memcpy(response, header, hdrLen);
   memcpy(response + hdrLen, text, len);
   response[responseLen - 1] = '\0';
-  printf("%s\n", response);
+  printf("RESPONSE:\n%s\n################\n", response);
   return write(fd, response, responseLen);
 }
 
-int http_parseRequest(int fd) {
-  char buf[1500];
-  char *saveptr, *req, *url;
-  recv(fd, buf, 1500, 0);
-  printf("%s\n", buf);
 
-  req = strtok_r(buf, " ", &saveptr);
-  printf("Request:%s\n", req);
-  if (strncmp(req, "GET", 3) != 0) {
-    printf("We only accept gets\n");
-    return 0;
+void request_addParam(Request *r, Param *p) {
+  if (r->param == NULL) r->param = p;
+  else {
+    Param *np = r->param;
+    while (np->next != NULL) np = np->next;
+    np->next = p;
   }
-  url = strtok_r(NULL, " ", &saveptr);
-  printf("URL: %s\n", url);
-  return 1;
-
 }
 
-int main(int argc, char const *argv[])
-{
-    int listenfd = 0, connfd = 0;
+Request *request_new() {
+  Request *r = (Request *) malloc (sizeof(Request));
+  if (r == NULL) return NULL;
+  memset(r->buf, '\0', REQUEST_SIZE);
+  r->type = NULL;
+  r->url = NULL;
+  r->param = NULL;
+  return r;
+}
+
+void request_free(Request *r) {
+  if (r == NULL) return;
+  /* Free parameter list */
+  Param *p, *q;
+  p = r->param;
+  while (p != NULL) {
+    q = p;
+    p = q->next;
+    free(q);
+  }
+  /* Free rest of struct */
+  free(r);
+}
+
+Request *http_parseRequest(int fd) {
+  Request *r = request_new();
+  if (r == NULL) return NULL;
+  if (recv(fd, r->buf, REQUEST_SIZE, 0) == -1) {
+    request_free(r);
+    return NULL;
+  }
+  printf("%s\n", r->buf);
+  /* Save pointers for strtok_r */
+  char *hdrsave, *urlsave, *querysave;
+  /* Get request type (GET, POST) */
+  r->type = strtok_r(r->buf, " ", &hdrsave);
+  printf("Request:%s\n", r->type);
+  if (strncmp(r->type, "GET", 3) != 0) {
+    printf("We only accept gets\n");
+    request_free(r);
+    return NULL;
+  }
+  /* Get request URL + params */
+  r->url = strtok_r(NULL, " ", &hdrsave);
+  /* Check if we have a list of query parameters and seperate */
+  char *url = strtok_r(r->url, "?", &urlsave);
+  printf("URL: %s\n", url);
+  if (url) r->url = url;
+  else return r;
+
+  /* Get query param half */
+  r->paramStr = strtok_r(NULL, "?", &urlsave);
+  if (r->paramStr == NULL) return r;
+  printf("paramstr:%s\n", r->paramStr);
+  /* Do we have more than one? (Split by &) */
+  char *q = strtok_r(r->paramStr, "&", &urlsave);
+  if (q == NULL) q = r->paramStr;
+
+  /* Retrieve params */
+  Param *p, *lastP;
+  do {
+    p = (Param *) malloc(sizeof(Param));
+    if (p == NULL) {
+      request_free(r);
+      return NULL;
+    }
+    /* Key, value pairs are split by = */
+    p->key = strtok_r(q, "=", &querysave);
+    p->value = strtok_r(NULL, "=", &querysave);
+    p->next = NULL;
+    printf("%s:%s\n", p->key, p->value);
+    /* Add to param list */
+    if (r->param == NULL) r->param = p;
+    else lastP->next = p;
+    lastP = p;  /* Used for next iteration, makes insertion O(1) */
+  } while((q = strtok_r(NULL, "&", &urlsave))); /* Next param */
+  return r;
+}
+
+int main(int argc, char const *argv[]) {
+    int listenfd, connfd;
     struct sockaddr_in serv_addr;
-
-    char sendBuff[1025];
-
+    int yes = 1;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    memset(sendBuff, '0', sizeof(sendBuff));
+    memset(&serv_addr, '\0', sizeof serv_addr);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000);
-
-    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
+    
+    // lose the pesky "Address already in use" error message
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+    if (bind(listenfd, (struct sockaddr*) &serv_addr, sizeof serv_addr) == -1) {
+      perror("bind");
+      exit(1);
+    }
     listen(listenfd, 10);
 
-    while(1)
-    {
+    while(1) {
         connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
-        printf("Got new connection\n");
-        http_parseRequest(connfd);
+        Request *r = http_parseRequest(connfd);
+        request_free(r);
         http_response(connfd, 200, "Hello world", 11);
-        // ticks = time(NULL);
-        // snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-
-
         close(connfd);
      }
 }
